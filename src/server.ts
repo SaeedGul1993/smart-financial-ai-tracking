7eba18d98b1f96cef4ca91e4f68f60715a4be94d
SaeedGul1993 <saeedgulkhan1993@gmail.com>
2026-08-10 22:05:29 +0500

    changes pushed

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import app from "./app";
 import { startBudgetReminderCron } from "./cron/budget-reminder.cron";
 import { checkRecurringExpense } from "./cron/recurring-expense.cron";
 
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT);
 startBudgetReminderCron();
 checkRecurringExpense();
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
