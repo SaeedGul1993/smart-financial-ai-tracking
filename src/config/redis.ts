4e58f53581382cd9f7a62e4fc5027f6f35860bce
SaeedGul1993 <saeedgulkhan1993@gmail.com>
2026-08-10 21:01:14 +0500

    add production worker process

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,17 @@
 import { Redis } from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: parseInt(process.env.REDIS_PORT || "6379"),
+if (!process.env.REDIS_URL) {
+  throw new Error("REDIS_URL is not configured");
+}
+
+export const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
 });
+
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST,
+//   port: parseInt(process.env.REDIS_PORT || "6379"),
+//   maxRetriesPerRequest: null,
+// });
 
 export default redis;
