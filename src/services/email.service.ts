8e2d536d37469f73b71aa651861ea48059dcadd5
SaeedGul1993 <saeedgulkhan1993@gmail.com>
2026-08-12 00:34:50 +0500

    feat:brevo email service added

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,38 +1,13 @@
-// import { Resend } from "resend";
-import nodemailer from "nodemailer";
+import { BrevoClient } from "@getbrevo/brevo";
+const brevo = new BrevoClient({ apiKey: process.env.BREVO_API_KEY! });
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_APP_PASSWORD,
-  },
-});
 export class EmailService {
   async sendEmail(email: string, subject: string, html: string) {
-    await transporter.sendMail({
-      from: process.env.SMTP_USER,
-      to: email,
+    await brevo.transactionalEmails.sendTransacEmail({
+      sender: { name: "Smart Finance", email: process.env.BREVO_SENDER_EMAIL! },
+      to: [{ email }],
       subject,
-      html,
+      htmlContent: html,
     });
   }
 }
-
-// import { Resend } from "resend";
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// export class EmailService {
-//   async sendEmail(email: string, subject: string, html: string) {
-//     return resend.emails.send({
-//       from: process.env.EMAIL_FROM || "noreply@budgetly.com",
-//       to: email,
-//       subject,
-//       html,
-//     });
-//   }
-// }
